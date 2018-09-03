4f6fac29d6d17820ee253b7d19e7c596b29631a6
Bruno Silva <brunocs86@outlook.com>
2018-09-03 11:59:19 -0300

    Delete conecta.php

--- conecta.php
+++
@@ -1,7 +0,0 @@
-<?php
-# $conexao = mysqli_connect("mysql.hostinger.com.br", "u548680052_loja", "lojaloja", "u548680052_loja");
-
-$conexao = mysqli_connect("localhost", "root", "aqm0hpv", "loja");
-if (mysqli_connect_errno()) {
-    echo "Failed to connect to MySQL: " . mysqli_connect_error();
-}
